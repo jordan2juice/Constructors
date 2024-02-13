b3fbfb9b35880ae0a006d8028a944545187220bd
Jordan W. Toujouse <146011829+jordan2juice@users.noreply.github.com>
2024-02-12 21:08:01 -0600

    added comments.

--- scripy.js
+++ scripy.js
@@ -1,13 +1,17 @@
+// created the function for the Television
+
 function Television(powerStatus, channel) {
-  this.powerStatus = powerStatus;
-  this.channel = channel;
+  this.powerStatus = powerStatus; //  on or off
+  this.channel = channel; // current channel
 }
 
+//  method to turn on the television
 Television.prototype.turnOn = function () {
   this.powerStatus = true;
   console.log("The  television is on!");
 };
 
+//  method to go channel up
 Television.prototype.channelUp = function () {
   this.channel++;
   if (this.channel > 100) {
@@ -16,6 +20,7 @@ Television.prototype.channelUp = function () {
   console.log("Current Channel: " + this.channel);
 };
 
+// method to go channel down
 Television.prototype.channelDown = function () {
   this.channel--;
   if (this.channel < 1) {
@@ -24,11 +29,13 @@ Television.prototype.channelDown = function () {
   console.log("Current Channel:" + this.channel);
 };
 
+// the get status of the TV. It will return whether the tv is turned on or not.
 Television.prototype.getState = function () {
   console.log(this.powerStatus ? "TV is ON" : "TV is OFF");
   console.log(this.channel);
 };
 
+//  multiple  objects can be created using the constructor and then methods can be called upon them.
 const myTelevision = new Television(true, 25);
 const myTelevision2 = new Television(false, 86);
 const myTelevision3 = new Television(true, 96);
